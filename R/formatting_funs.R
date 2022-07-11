98ad2d2cdf98484c3f72536754890039f3267c55
danni-tu <dannitu34@gmail.com>
2022-07-11 11:36:51 -0400

    adding initial function

---
+++ R/formatting_funs.R
@@ -0,0 +1,14 @@
+#' Print new line in HTML files
+#'
+#' This function prints a new line in HTML files, which is useful when printing other things such as headers or plots.
+#'
+#' @return Returns nothing except some spaces!!
+#'
+#' @examples
+#' nl()
+#'
+#' @export
+nl <- function(){
+  cat("  \n")
+  cat("  \n")
+}
